aa0ef9cee1e65099fad1f8c137bddfcb8f448538
LatvianModder <latvianmodder@gmail.com>
2020-10-09 16:15:50 +0300

    Added smithing recipe

--- src/main/java/dev/latvian/kubejs/recipe/KubeJSRecipeEventHandler.java
+++ src/main/java/dev/latvian/kubejs/recipe/KubeJSRecipeEventHandler.java
@@ -3,6 +3,7 @@ package dev.latvian.kubejs.recipe;
 import dev.latvian.kubejs.recipe.minecraft.CookingRecipeJS;
 import dev.latvian.kubejs.recipe.minecraft.ShapedRecipeJS;
 import dev.latvian.kubejs.recipe.minecraft.ShapelessRecipeJS;
+import dev.latvian.kubejs.recipe.minecraft.SmithingRecipeJS;
 import dev.latvian.kubejs.recipe.minecraft.StonecuttingRecipeJS;
 import net.minecraftforge.common.MinecraftForge;
 
@@ -26,5 +27,6 @@ public class KubeJSRecipeEventHandler
 		event.register("minecraft:blasting", CookingRecipeJS::new);
 		event.register("minecraft:smoking", CookingRecipeJS::new);
 		event.register("minecraft:campfire_cooking", CookingRecipeJS::new);
+		event.register("minecraft:smithing", SmithingRecipeJS::new);
 	}
 }
