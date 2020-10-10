2e29449a3938226bd6461f40b82dfbcdd6216ad0
LatvianModder <latvianmodder@gmail.com>
2020-10-10 20:11:24 +0300

    Fixed Mekanism metallurgic infusing and sawing recipes

--- src/main/java/dev/latvian/kubejs/recipe/mekanism/MekanismRecipes.java
+++ src/main/java/dev/latvian/kubejs/recipe/mekanism/MekanismRecipes.java
@@ -13,8 +13,8 @@ public class MekanismRecipes
 		event.register("mekanism:enriching", MekanismMachineRecipeJS::new);
 		event.register("mekanism:purifying", () -> new MekanismMachineRecipeJS("itemInput", "output"));
 		event.register("mekanism:injecting", () -> new MekanismMachineRecipeJS("itemInput", "output"));
-		event.register("mekanism:metallurgic_infusing", () -> new MekanismMachineRecipeJS("itemInput", "output"));
-		event.register("mekanism:sawing", () -> new MekanismMachineRecipeJS("itemInput", "mainOutput"));
+		event.register("mekanism:metallurgic_infusing", MekanismMetallurgicInfusingRecipeJS::new);
+		event.register("mekanism:sawing", () -> new MekanismMachineRecipeJS("input", "mainOutput"));
 		event.register("mekanism:combining", MekanismCombiningRecipeJS::new);
 	}
 }
