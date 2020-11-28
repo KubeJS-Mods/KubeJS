c179c63cad2339223d3096002c52c76a3aa1643d
LatvianModder <latvianmodder@gmail.com>
2020-11-28 19:01:10 +0200

    Removed mekanism recipes. Use KubeJS Mekanism mod

--- forge/src/main/java/dev/latvian/kubejs/integration/IntegrationManager.java
+++ forge/src/main/java/dev/latvian/kubejs/integration/IntegrationManager.java
@@ -2,7 +2,6 @@ package dev.latvian.kubejs.integration;
 
 import dev.latvian.kubejs.integration.gamestages.GameStagesIntegration;
 import dev.latvian.kubejs.recipe.create.CreateRecipes;
-import dev.latvian.kubejs.recipe.mekanism.MekanismRecipes;
 import dev.latvian.kubejs.recipe.silentsmek.SilentsMechanismsRecipes;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModList;
@@ -22,11 +21,6 @@ public class IntegrationManager
 		if (ModList.get().isLoaded("create") && !ModList.get().isLoaded("kubejs_create"))
 		{
 			MinecraftForge.EVENT_BUS.register(CreateRecipes.class);
-		}
-
-		if (ModList.get().isLoaded("mekanism") && !ModList.get().isLoaded("kubejs_mekanism"))
-		{
-			MinecraftForge.EVENT_BUS.register(MekanismRecipes.class);
 		}
 
 		if (ModList.get().isLoaded("silents_mechanisms") && !ModList.get().isLoaded("kubejs_silents_mechanisms"))
