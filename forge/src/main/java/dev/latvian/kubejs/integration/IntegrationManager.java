fcb071661bd59a70ce6a7a886f02431bdc3ade11
LatvianModder <latvianmodder@gmail.com>
2020-12-29 15:22:04 +0200

    Removed Create integration, it was very outdated and now has been replaced with KubeJS Create addon mod

--- forge/src/main/java/dev/latvian/kubejs/integration/IntegrationManager.java
+++ forge/src/main/java/dev/latvian/kubejs/integration/IntegrationManager.java
@@ -1,7 +1,6 @@
 package dev.latvian.kubejs.integration;
 
 import dev.latvian.kubejs.integration.gamestages.GameStagesIntegration;
-import dev.latvian.kubejs.recipe.create.CreateRecipes;
 import dev.latvian.kubejs.recipe.silentsmek.SilentsMechanismsRecipes;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModList;
@@ -16,11 +15,6 @@ public class IntegrationManager
 		if (ModList.get().isLoaded("gamestages"))
 		{
 			GameStagesIntegration.init();
-		}
-
-		if (ModList.get().isLoaded("create") && !ModList.get().isLoaded("kubejs_create"))
-		{
-			MinecraftForge.EVENT_BUS.register(CreateRecipes.class);
 		}
 
 		if (ModList.get().isLoaded("silents_mechanisms") && !ModList.get().isLoaded("kubejs_silents_mechanisms"))
