7cdac61b051a8e6d4b7365b4c2a13de9e06c0ad2
LatvianModder <latvianmodder@gmail.com>
2021-01-20 17:11:29 +0200

    Fixed AE2 block placing crash

--- common/src/main/java/dev/latvian/kubejs/block/BlockPlaceEventJS.java
+++ common/src/main/java/dev/latvian/kubejs/block/BlockPlaceEventJS.java
@@ -3,10 +3,13 @@ package dev.latvian.kubejs.block;
 import dev.latvian.kubejs.entity.EntityEventJS;
 import dev.latvian.kubejs.entity.EntityJS;
 import dev.latvian.kubejs.world.BlockContainerJS;
+import dev.latvian.kubejs.world.WorldJS;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+
+import javax.annotation.Nullable;
 
 /**
  * @author LatvianModder
@@ -18,7 +21,7 @@ public class BlockPlaceEventJS extends EntityEventJS
 	private final BlockPos pos;
 	private final BlockState state;
 
-	public BlockPlaceEventJS(Entity entity, Level world, BlockPos pos, BlockState state)
+	public BlockPlaceEventJS(@Nullable Entity entity, Level world, BlockPos pos, BlockState state)
 	{
 		this.entity = entity;
 		this.world = world;
@@ -33,9 +36,15 @@ public class BlockPlaceEventJS extends EntityEventJS
 	}
 
 	@Override
+	public WorldJS getWorld()
+	{
+		return worldOf(world);
+	}
+
+	@Override
 	public EntityJS getEntity()
 	{
-		return entityOf(entity);
+		return entity == null ? null : entityOf(entity);
 	}
 
 	public BlockContainerJS getBlock()
