b52ee6a2368be897cc7d689f28ab2beb5b7aa009
LatvianModder <latvianmodder@gmail.com>
2021-06-27 15:04:33 +0300

    Added event.shift, ctrl, alt in item.tooltip event

--- common/src/main/java/dev/latvian/kubejs/item/custom/ItemToolTierEventJS.java
+++ common/src/main/java/dev/latvian/kubejs/item/custom/ItemToolTierEventJS.java
@@ -3,6 +3,7 @@ package dev.latvian.kubejs.item.custom;
 import dev.latvian.kubejs.event.EventJS;
 import dev.latvian.kubejs.item.ItemBuilder;
 import dev.latvian.kubejs.item.ModifiedToolTier;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.world.item.Tiers;
 
 import java.util.function.Consumer;
@@ -15,5 +16,17 @@ public class ItemToolTierEventJS extends EventJS {
 		ModifiedToolTier t = new ModifiedToolTier(Tiers.IRON);
 		tier.accept(t);
 		ItemBuilder.TOOL_TIERS.put(id, t);
+	}
+
+	public boolean isShift() {
+		return Screen.hasShiftDown();
+	}
+
+	public boolean isCtrl() {
+		return Screen.hasControlDown();
+	}
+
+	public boolean isAlt() {
+		return Screen.hasAltDown();
 	}
 }
