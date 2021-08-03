3be88bcace289c3a9a3ca6ec5c77854e91d8b149
LatvianModder <latvianmodder@gmail.com>
2021-08-03 15:18:25 +0300

    Added PlayerStatsJS#getBlocksMined(block), getItemsCrafted(item), getItemsUsed(item), getItemsBroken(item), getItemsPickedUp(item), getItemsDropped(item), getKilled(entityType), getKilledBy(entityType)

--- common/src/main/java/dev/latvian/kubejs/player/PlayerStatsJS.java
+++ common/src/main/java/dev/latvian/kubejs/player/PlayerStatsJS.java
@@ -2,7 +2,11 @@ package dev.latvian.kubejs.player;
 
 import dev.latvian.kubejs.util.UtilsJS;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.stats.Stats;
 import net.minecraft.stats.StatsCounter;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 
 /**
  * @author LatvianModder
@@ -30,5 +34,37 @@ public class PlayerStatsJS {
 
 	public void add(ResourceLocation id, int value) {
 		statFile.increment(player.minecraftPlayer, UtilsJS.getStat(id), value);
+	}
+
+	public int getBlocksMined(Block block) {
+		return statFile.getValue(Stats.BLOCK_MINED.get(block));
+	}
+
+	public int getItemsCrafted(Item item) {
+		return statFile.getValue(Stats.ITEM_CRAFTED.get(item));
+	}
+
+	public int getItemsUsed(Item item) {
+		return statFile.getValue(Stats.ITEM_USED.get(item));
+	}
+
+	public int getItemsBroken(Item item) {
+		return statFile.getValue(Stats.ITEM_BROKEN.get(item));
+	}
+
+	public int getItemsPickedUp(Item item) {
+		return statFile.getValue(Stats.ITEM_PICKED_UP.get(item));
+	}
+
+	public int getItemsDropped(Item item) {
+		return statFile.getValue(Stats.ITEM_DROPPED.get(item));
+	}
+
+	public int getKilled(EntityType<?> entity) {
+		return statFile.getValue(Stats.ENTITY_KILLED.get(entity));
+	}
+
+	public int getKilledBy(EntityType<?> entity) {
+		return statFile.getValue(Stats.ENTITY_KILLED_BY.get(entity));
 	}
 }
