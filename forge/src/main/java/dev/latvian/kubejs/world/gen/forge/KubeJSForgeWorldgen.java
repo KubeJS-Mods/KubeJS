4bdb14cb6d8ce8cf56ae78b21e87fdbaadb5a26e
LatvianModder <latvianmodder@gmail.com>
2021-08-07 14:30:19 +0300

    Reverted previous change

--- forge/src/main/java/dev/latvian/kubejs/world/gen/forge/KubeJSForgeWorldgen.java
+++ forge/src/main/java/dev/latvian/kubejs/world/gen/forge/KubeJSForgeWorldgen.java
@@ -1,5 +1,7 @@
 package dev.latvian.kubejs.world.gen.forge;
 
+import dev.latvian.kubejs.KubeJSEvents;
+import dev.latvian.kubejs.script.ScriptType;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,7 +14,7 @@ import net.minecraftforge.fml.common.Mod;
 public class KubeJSForgeWorldgen {
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public static void onBiomesLoad(BiomeLoadingEvent event) {
-		//new WorldgenRemoveEventJSForge(event).post(ScriptType.STARTUP, KubeJSEvents.WORLDGEN_REMOVE);
-		//new WorldgenAddEventJSForge(event).post(ScriptType.STARTUP, KubeJSEvents.WORLDGEN_ADD);
+		new WorldgenRemoveEventJSForge(event).post(ScriptType.STARTUP, KubeJSEvents.WORLDGEN_REMOVE);
+		new WorldgenAddEventJSForge(event).post(ScriptType.STARTUP, KubeJSEvents.WORLDGEN_ADD);
 	}
 }
