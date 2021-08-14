90a76b53aa3da9435e67274459263b6e6610c082
LatvianModder <latvianmodder@gmail.com>
2021-08-14 21:43:25 +0300

    Removed player.check_login event for now, it's causing issues

--- common/src/main/java/dev/latvian/kubejs/mixin/common/PlayerListMixin.java
+++ common/src/main/java/dev/latvian/kubejs/mixin/common/PlayerListMixin.java
@@ -1,23 +1,11 @@
 package dev.latvian.kubejs.mixin.common;
 
-import com.mojang.authlib.GameProfile;
-import dev.latvian.kubejs.KubeJSEvents;
-import dev.latvian.kubejs.player.CheckPlayerLoginEventJS;
-import dev.latvian.kubejs.script.ScriptType;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.players.PlayerList;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.net.SocketAddress;
 
 @Mixin(PlayerList.class)
 public abstract class PlayerListMixin {
+	/*
 	@Shadow
 	@Final
 	private MinecraftServer server;
@@ -31,4 +19,5 @@ public abstract class PlayerListMixin {
 			}
 		}
 	}
+	*/
 }
