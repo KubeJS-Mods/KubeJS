f128f305fe662ba88911038cf38e2868390c988c
LatvianModder <latvianmodder@gmail.com>
2021-08-25 10:58:59 +0300

    Added botaniaRunicAltar recipe support

---
+++ common/src/main/java/dev/latvian/kubejs/recipe/mod/BotaniaRunicAltarRecipeJS.java
@@ -0,0 +1,47 @@
+package dev.latvian.kubejs.recipe.mod;
+
+import com.google.gson.JsonArray;
+import dev.latvian.kubejs.item.ingredient.IngredientJS;
+import dev.latvian.kubejs.recipe.RecipeJS;
+import dev.latvian.kubejs.util.ListJS;
+
+/**
+ * @author LatvianModder
+ */
+public class BotaniaRunicAltarRecipeJS extends RecipeJS {
+	@Override
+	public void create(ListJS args) {
+		outputItems.add(parseResultItem(args.get(0)));
+		inputItems.addAll(parseIngredientItemList(args.get(1)));
+		json.addProperty("mana", args.size() >= 3 ? (Number) args.get(2) : 1000);
+	}
+
+	@Override
+	public void deserialize() {
+		outputItems.add(parseResultItem(json.get("output")));
+		inputItems.addAll(parseIngredientItemList(json.get("ingredients")));
+	}
+
+	public BotaniaRunicAltarRecipeJS mana(int t) {
+		json.addProperty("mana", t);
+		save();
+		return this;
+	}
+
+	@Override
+	public void serialize() {
+		if (serializeOutputs) {
+			json.add("output", outputItems.get(0).toResultJson());
+		}
+
+		if (serializeInputs) {
+			JsonArray a = new JsonArray();
+
+			for (IngredientJS in : inputItems) {
+				a.add(in.toJson());
+			}
+
+			json.add("ingredients", a);
+		}
+	}
+}
