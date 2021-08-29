5a5c405ce6422ce8d6cd86532cf986c65cef0566
Ralph Schuler <ralph.schuler@nyphon.de>
2021-08-29 20:29:55 +0200

    Update WorldJS.java

--- common/src/main/java/dev/latvian/kubejs/world/WorldJS.java
+++ common/src/main/java/dev/latvian/kubejs/world/WorldJS.java
@@ -110,6 +110,11 @@ public abstract class WorldJS implements WithAttachedData {
 
 	public abstract PlayerDataJS getPlayerData(Player player);
 
+	/**
+     * get Entity
+	 * @param e Entity
+     * @return EntityJS
+     */
 	@Nullable
 	public EntityJS getEntity(@Nullable Entity e) {
 		if (e == null) {
@@ -127,12 +132,20 @@ public abstract class WorldJS implements WithAttachedData {
 		return new EntityJS(this, e);
 	}
 
+	/**
+     * get living Entity
+     * @return LivingEntityJS
+     */
 	@Nullable
 	public LivingEntityJS getLivingEntity(@Nullable Entity entity) {
 		EntityJS e = getEntity(entity);
 		return e instanceof LivingEntityJS ? (LivingEntityJS) e : null;
 	}
 
+	/**
+     * get Player
+     * @return PlayerJS
+     */
 	@Nullable
 	public PlayerJS getPlayer(@Nullable Entity entity) {
 		if (!(entity instanceof Player)) {
@@ -143,22 +156,43 @@ public abstract class WorldJS implements WithAttachedData {
 		return e instanceof PlayerJS ? (PlayerJS) e : null;
 	}
 
+	//TODO
 	public EntityArrayList createEntityList(Collection<? extends Entity> entities) {
 		return new EntityArrayList(this, entities);
 	}
 
+	/**
+     * get Players
+     * @return EntityArrayList
+     */
 	public EntityArrayList getPlayers() {
 		return createEntityList(minecraftWorld.players());
 	}
 
+	/**
+     * get Entities
+     * @return EntityArrayList
+     */
 	public EntityArrayList getEntities() {
 		return new EntityArrayList(this, 0);
 	}
 
+	/**
+     * create Explosion
+     * @param x float
+     * @param y float
+     * @param z float
+     * @return ExplosionJS
+     */
 	public ExplosionJS createExplosion(double x, double y, double z) {
 		return new ExplosionJS(minecraftWorld, x, y, z);
 	}
 
+	/**
+     * create Entity
+     * @param id ResourceLocation
+     * @return EntityJS
+     */
 	@Nullable
 	public EntityJS createEntity(ResourceLocation id) {
 		EntityType<?> type = Registry.ENTITY_TYPE.get(id);
@@ -170,6 +204,14 @@ public abstract class WorldJS implements WithAttachedData {
 		return getEntity(type.create(minecraftWorld));
 	}
 
+	/**
+     * spawn Lightning
+     * @param x float
+     * @param y float
+     * @param z float
+     * @param effectOnly bool
+	 * @param player EntityJS (default = null)
+     */
 	public void spawnLightning(double x, double y, double z, boolean effectOnly, @Nullable EntityJS player) {
 		if (minecraftWorld instanceof ServerLevel) {
 			LightningBolt e = EntityType.LIGHTNING_BOLT.create(minecraftWorld);
@@ -179,10 +221,24 @@ public abstract class WorldJS implements WithAttachedData {
 		}
 	}
 
+	/**
+     * spawn Lightning
+     * @param x float
+     * @param y float
+     * @param z float
+     * @param effectOnly bool
+     */
 	public void spawnLightning(double x, double y, double z, boolean effectOnly) {
 		spawnLightning(x, y, z, effectOnly, null);
 	}
 
+	/**
+     * spawn Fireworks
+     * @param x float
+     * @param y float
+     * @param z float
+     * @param f Fireworks
+     */
 	public void spawnFireworks(double x, double y, double z, FireworksJS f) {
 		minecraftWorld.addFreshEntity(f.createFireworkRocket(minecraftWorld, x, y, z));
 	}
