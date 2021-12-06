d218a2ada0556ab295a683e780e5737791ea0d10
Max <maxh2709@gmail.com>
2021-12-06 20:33:42 +0100

    🚧 Mark KubeJSInitializer as deprecated / experimental for the time being

--- fabric/src/main/java/dev/latvian/mods/kubejs/KubeJSInitializer.java
+++ fabric/src/main/java/dev/latvian/mods/kubejs/KubeJSInitializer.java
@@ -1,5 +1,13 @@
 package dev.latvian.mods.kubejs;
 
+import org.jetbrains.annotations.ApiStatus;
+
+/**
+ * @deprecated Use {@link KubeJSPlugin}s instead.
+ * This API may change or even be removed entirely in the future.
+ */
+@Deprecated
+@ApiStatus.Experimental
 @FunctionalInterface
 public interface KubeJSInitializer {
 	void onKubeJSInitialization();
