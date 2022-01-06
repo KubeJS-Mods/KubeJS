ee687f7e2ce3839228c83777e37a645590153ee4
Max <maxh2709@gmail.com>
2022-01-06 17:22:15 +0100

    🐛 Fix using JEI internals in JEI_INFORMATION, remove JEI compile dep

--- forge/src/main/java/dev/latvian/mods/kubejs/integration/forge/jei/InformationJEIEventJS.java
+++ forge/src/main/java/dev/latvian/mods/kubejs/integration/forge/jei/InformationJEIEventJS.java
@@ -6,46 +6,50 @@ import dev.latvian.mods.kubejs.item.ingredient.IngredientJS;
 import dev.latvian.mods.kubejs.text.Text;
 import dev.latvian.mods.kubejs.util.ListJS;
 import mezz.jei.api.constants.VanillaTypes;
-import mezz.jei.api.ingredients.IIngredientHelper;
 import mezz.jei.api.ingredients.IIngredientType;
-import mezz.jei.api.runtime.IIngredientManager;
-import mezz.jei.plugins.jei.info.IngredientInfoRecipe;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.Util;
 import net.minecraft.network.chat.Component;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.HashSet;
 
 /**
  * @author LatvianModder
  */
 
 public class InformationJEIEventJS extends EventJS {
-	private final IIngredientManager manager;
-	private final List<IngredientInfoRecipe<?>> recipes;
+	private final IRecipeRegistration registration;
 
-	public InformationJEIEventJS(IIngredientManager m, List<IngredientInfoRecipe<?>> l) {
-		manager = m;
-		recipes = l;
+	public InformationJEIEventJS(IRecipeRegistration reg) {
+		registration = reg;
 	}
 
 	public void add(Object o, Text[] s) {
-		recipes.addAll(IngredientInfoRecipe.create(
-				IngredientJS.of(o).getStacks().stream().map(ItemStackJS::getItemStack).collect(Collectors.toList()),
+		registration.addIngredientInfo(
+				IngredientJS.of(o).getStacks().stream().map(ItemStackJS::getItemStack).toList(),
 				VanillaTypes.ITEM,
-				Arrays.stream(s).map(Text::component).toArray(Component[]::new)));
+				toComponent(s)
+		);
 	}
 
 	public <T> void addForType(IIngredientType<T> type, Object o, Text[] s) {
-        var targets = ListJS.orSelf(o).stream().map(String::valueOf).collect(Collectors.toSet());
-        var helper = manager.getIngredientHelper(type);
-		recipes.addAll(IngredientInfoRecipe.create(
+		var targets = new HashSet<>(ListJS.orSelf(o).map(String::valueOf));
+		var manager = registration.getIngredientManager();
+		var helper = manager.getIngredientHelper(type);
+		registration.addIngredientInfo(
 				manager.getAllIngredients(type)
 						.stream()
 						.filter(t -> targets.contains(helper.getWildcardId(t)))
-						.collect(Collectors.toList()),
-				type, Arrays.stream(s).map(Text::component).toArray(Component[]::new)));
+						.toList(),
+				type, toComponent(s));
+	}
+
+	public static Component[] toComponent(Text[] s) {
+		return Util.make(new Component[s.length], arr -> {
+			for (var i = 0; i < s.length; i++) {
+				arr[i] = s[i].component();
+			}
+		});
 	}
 
 }
