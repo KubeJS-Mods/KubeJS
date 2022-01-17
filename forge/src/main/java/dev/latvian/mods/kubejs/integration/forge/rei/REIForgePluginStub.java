93c5fc4b78964a5ca190ea9a431c31b071892cbd
Max <maxh2709@gmail.com>
2022-01-17 21:14:07 +0100

    🐛 Mark REI plugin as client only

--- forge/src/main/java/dev/latvian/mods/kubejs/integration/forge/rei/REIForgePluginStub.java
+++ forge/src/main/java/dev/latvian/mods/kubejs/integration/forge/rei/REIForgePluginStub.java
@@ -2,7 +2,8 @@ package dev.latvian.mods.kubejs.integration.forge.rei;
 
 import dev.latvian.mods.kubejs.integration.rei.KubeJSREIPlugin;
 import me.shedaniel.rei.forge.REIPlugin;
+import net.minecraftforge.api.distmarker.Dist;
 
-@REIPlugin
+@REIPlugin(Dist.CLIENT)
 public class REIForgePluginStub extends KubeJSREIPlugin {
 }
