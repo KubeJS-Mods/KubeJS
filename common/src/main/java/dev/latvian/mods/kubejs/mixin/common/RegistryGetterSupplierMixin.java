4403801b97a5f39800272467f70f98adbb6fc799
LLytho <29131229+LLytho@users.noreply.github.com>
2022-03-02 15:14:42 +0100

    Mixin into createRegistryGetter instead of anonymous class

--- common/src/main/java/dev/latvian/mods/kubejs/mixin/common/RegistryGetterSupplierMixin.java
+++ common/src/main/java/dev/latvian/mods/kubejs/mixin/common/RegistryGetterSupplierMixin.java
@@ -2,30 +2,42 @@ package dev.latvian.mods.kubejs.mixin.common;
 
 import dev.latvian.mods.kubejs.core.RegistryGetterKJS;
 import net.minecraft.core.Registry;
+import net.minecraft.resources.RegistryReadOps;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-// FIXME: THISISTERRIBLETHISISTERRIBLETHISISTERRIBLETHISISTERRIBLETHISISTERRIBLE
-@Mixin(targets = "net.minecraft.resources.RegistryReadOps$1")
+import java.util.function.Supplier;
+
+@Mixin(RegistryReadOps.class)
 public abstract class RegistryGetterSupplierMixin<E> implements RegistryGetterKJS<E> {
-	@Shadow
-	@Final
-	Registry<E> val$registry;
+	@Inject(method = "createRegistryGetter", at = @At(value = "RETURN"), cancellable = true)
+	private static <E> void delegateRegistryGetter(Registry<E> registry, ResourceKey<E> resourceKey, CallbackInfoReturnable<Supplier<E>> cir) {
+		Supplier<E> delegate = cir.getReturnValue();
+		RegistryGetterKJS<E> registryGetter = new RegistryGetterKJS<>() {
+			@Override
+			public E get() {
+				return delegate.get();
+			}
 
-	@Shadow
-	@Final
-	ResourceKey<E> val$elementKey;
+			@Override
+			public Registry<E> getRegistry() {
+				return registry;
+			}
 
-	@Override
-	public Registry<E> getRegistry() {
-		return val$registry;
-	}
+			@Override
+			public ResourceLocation getId() {
+				return resourceKey.location();
+			}
 
-	@Override
-	public ResourceLocation getId() {
-		return val$elementKey.location();
+			@Override
+			public String toString() {
+				return delegate.toString();
+			}
+		};
+		cir.setReturnValue(registryGetter);
 	}
 }
