528f2fda587bc9dd7b51ac20593e178737d81017
Prunoideae <a455167189@163.com>
2022-04-16 09:05:48 +0800

    added crop block support

---
+++ common/src/main/java/dev/latvian/mods/kubejs/block/SeedItemBuilder.java
@@ -0,0 +1,20 @@
+package dev.latvian.mods.kubejs.block;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemNameBlockItem;
+
+/**
+ * @author Prunoideae
+ */
+public class SeedItemBuilder extends BlockItemBuilder {
+
+	public SeedItemBuilder(ResourceLocation i) {
+		super(i);
+	}
+
+	@Override
+	public Item createObject() {
+		return new ItemNameBlockItem(blockBuilder.get(), createItemProperties());
+	}
+}
