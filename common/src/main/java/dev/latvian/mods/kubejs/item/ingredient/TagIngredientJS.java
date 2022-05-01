cb31c4bf741722c1dacacc5cb78ef548745b4832
Max <maxh2709@gmail.com>
2022-05-01 15:49:30 +0200

    Introduce (primitive) context object to TagIngredientJS

--- common/src/main/java/dev/latvian/mods/kubejs/item/ingredient/TagIngredientJS.java
+++ common/src/main/java/dev/latvian/mods/kubejs/item/ingredient/TagIngredientJS.java
@@ -1,7 +1,9 @@
 package dev.latvian.mods.kubejs.item.ingredient;
 
+import com.google.common.collect.Iterables;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import dev.latvian.mods.kubejs.KubeJS;
 import dev.latvian.mods.kubejs.item.ItemStackJS;
 import dev.latvian.mods.kubejs.recipe.RecipeExceptionJS;
 import dev.latvian.mods.kubejs.recipe.RecipeJS;
@@ -11,13 +13,16 @@ import net.minecraft.Util;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.Tag;
 import net.minecraft.tags.TagKey;
+import net.minecraft.tags.TagManager;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -26,13 +31,14 @@ import java.util.Set;
 public class TagIngredientJS implements IngredientJS {
 	// private static final Map<String, TagIngredientJS> tagIngredientCache = new HashMap<>();
 
+	public static Context context = Context.EMPTY;
+
 	public static TagIngredientJS createTag(String tag) {
 		return new TagIngredientJS(tag).validateTag();
-		// return tagIngredientCache.computeIfAbsent(tag, TagIngredientJS::new).validateTag();
 	}
 
-	public static void clearTagCache() {
-		// tagIngredientCache.clear();
+	public static void resetContext() {
+		context = Context.EMPTY;
 	}
 
 	private final TagKey<Item> tag;
@@ -48,7 +54,14 @@ public class TagIngredientJS implements IngredientJS {
 
 	public Iterable<Holder<Item>> getHolders() {
 		if (holders == null) {
-			holders = Registry.ITEM.getTagOrEmpty(tag);
+			if (context.areTagsBound()) {
+				// tags are bound, so we can cache the result from the registry
+				holders = Registry.ITEM.getTagOrEmpty(tag);
+			} else {
+				// tags have not been bound yet, so we can't trust that the tag won't change
+				// (for example as a result of other mods' modifications)
+				return context.getTag(tag);
+			}
 		}
 		return holders;
 	}
@@ -88,8 +101,6 @@ public class TagIngredientJS implements IngredientJS {
 
 	@Override
 	public ItemStackJS getFirst() {
-		validateTag();
-
 		for (var holder : getHolders()) {
 			return new ItemStackJS(new ItemStack(holder));
 		}
@@ -124,7 +135,7 @@ public class TagIngredientJS implements IngredientJS {
 	}
 
 	private TagIngredientJS validateTag() {
-		if (RecipeJS.itemErrors && isEmpty()) {
+		if (RecipeJS.itemErrors && context.isEmpty(tag)) {
 			throw new RecipeExceptionJS("Tag %s doesn't contain any items!".formatted(this)).error();
 		}
 		return this;
@@ -133,5 +144,67 @@ public class TagIngredientJS implements IngredientJS {
 	@Override
 	public Ingredient createVanillaIngredient() {
 		return Ingredient.of(tag);
+	}
+
+	public interface Context {
+		boolean isEmpty(TagKey<Item> tag);
+
+		boolean areTagsBound();
+
+		Iterable<Holder<Item>> getTag(TagKey<Item> tag);
+
+		Context EMPTY = new Context() {
+			@Override
+			public boolean isEmpty(TagKey<Item> tag) {
+				return true;
+			}
+
+			@Override
+			public boolean areTagsBound() {
+				return false;
+			}
+
+			@Override
+			public Iterable<Holder<Item>> getTag(TagKey<Item> tag) {
+				KubeJS.LOGGER.warn("Tried to get tag {} from an empty tag context!", tag.location());
+				return List.of();
+			}
+		};
+
+		Context REGISTRY = new Context() {
+			@Override
+			public boolean isEmpty(TagKey<Item> tag) {
+				return Registry.ITEM.getTag(tag).isEmpty();
+			}
+
+			@Override
+			public boolean areTagsBound() {
+				return true;
+			}
+
+			@Override
+			public Iterable<Holder<Item>> getTag(TagKey<Item> tag) {
+				return Registry.ITEM.getTagOrEmpty(tag);
+			}
+		};
+
+		static Context usingResult(TagManager.LoadResult<Item> manager) {
+			return new Context() {
+				@Override
+				public boolean isEmpty(TagKey<Item> tag) {
+					return Iterables.isEmpty(getTag(tag));
+				}
+
+				@Override
+				public boolean areTagsBound() {
+					return false;
+				}
+
+				@Override
+				public Iterable<Holder<Item>> getTag(TagKey<Item> tag) {
+					return manager.tags().getOrDefault(tag.location(), Tag.empty()).getValues();
+				}
+			};
+		}
 	}
 }
