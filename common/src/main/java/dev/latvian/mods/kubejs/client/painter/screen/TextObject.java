9cf964937b91dd5719e3f71daafb5040e3912bef
LatvianModder <latvianmodder@gmail.com>
2022-05-15 14:28:39 +0300

    Added textLines: [] support in text Painter object, fixed offsets

--- common/src/main/java/dev/latvian/mods/kubejs/client/painter/screen/TextObject.java
+++ common/src/main/java/dev/latvian/mods/kubejs/client/painter/screen/TextObject.java
@@ -4,61 +4,72 @@ import dev.latvian.mods.kubejs.client.painter.PainterObjectProperties;
 import dev.latvian.mods.kubejs.text.Text;
 import dev.latvian.mods.rhino.util.unit.FixedUnit;
 import dev.latvian.mods.rhino.util.unit.Unit;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.util.FormattedCharSequence;
 
 public class TextObject extends ScreenPainterObject {
-	private FormattedCharSequence text = FormattedCharSequence.EMPTY;
+	private FormattedCharSequence[] text = new FormattedCharSequence[0];
 	private boolean shadow = false;
 	private float scale = 1F;
 	private Unit color = PainterObjectProperties.WHITE_COLOR;
 	private boolean centered = false;
+	private float lineSpacing = 10F;
 
-	private float textWidth;
+	private float maxTextWidth = 0F;
+	private float[] textWidth = new float[0];
 
 	@Override
 	protected void load(PainterObjectProperties properties) {
 		super.load(properties);
 
-		text = Text.componentOf(properties.tag.get("text")).getVisualOrderText();
+		if (properties.tag.get("textLines") instanceof ListTag list) {
+			text = new FormattedCharSequence[list.size()];
+
+			for (int i = 0; i < list.size(); i++) {
+				text[i] = Text.componentOf(list.get(i)).getVisualOrderText();
+			}
+		} else {
+			text = new FormattedCharSequence[]{
+					Text.componentOf(properties.tag.get("text")).getVisualOrderText()
+			};
+		}
+
 		shadow = properties.getBoolean("shadow", shadow);
 		scale = properties.getFloat("scale", scale);
 		color = properties.getColor("color", color);
 		centered = properties.getBoolean("centered", centered);
+		lineSpacing = properties.getFloat("lineSpacing", lineSpacing);
+
+		textWidth = new float[text.length];
 	}
 
 	@Override
 	public void preDraw(ScreenPaintEventJS event) {
-		textWidth = event.font.getSplitter().stringWidth(text);
-		w = FixedUnit.of(textWidth * scale);
+		maxTextWidth = 0F;
+
+		for (int i = 0; i < text.length; i++) {
+			textWidth[i] = event.font.getSplitter().stringWidth(text[i]);
+			maxTextWidth = Math.max(maxTextWidth, textWidth[i]);
+		}
+
+		w = FixedUnit.of(maxTextWidth * scale);
 		h = FixedUnit.of(9F * scale);
 	}
 
 	@Override
 	public void draw(ScreenPaintEventJS event) {
-		var aw = w.get();
-		var ah = h.get();
-		var ax = event.alignX(x.get(), aw, alignX);
-		var ay = event.alignY(y.get(), ah, alignY);
+		var ax = event.alignX(x.get(), maxTextWidth, alignX);
+		var ay = event.alignY(y.get(), text.length * lineSpacing, alignY);
 		var az = z.get();
 
-		if (scale == 1F && az == 0F) {
-			if (centered) {
-				event.rawText(text, ax - textWidth / 2F, ay, color.getAsInt(), shadow);
-			} else {
-				event.rawText(text, ax, ay, color.getAsInt(), shadow);
-			}
-		} else {
-			event.push();
-			event.translate(ax, ay, az);
-			event.scale(scale);
+		event.push();
+		event.translate(ax, ay, az);
+		event.scale(scale);
 
-			if (centered) {
-				event.rawText(text, -textWidth / 2F, 0, color.getAsInt(), shadow);
-			} else {
-				event.rawText(text, 0, 0, color.getAsInt(), shadow);
-			}
-
-			event.pop();
+		for (int i = 0; i < text.length; i++) {
+			event.rawText(text[i], centered ? -(textWidth[i] / 2F) : 0, i * lineSpacing, color.getAsInt(), shadow);
 		}
+
+		event.pop();
 	}
 }
