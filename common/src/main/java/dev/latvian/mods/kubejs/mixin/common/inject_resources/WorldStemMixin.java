b7107efcf918bc9939e56a9bb7185cb5015d4e5b
Max <maxh2709@gmail.com>
2022-07-23 21:09:21 +0200

    Replace some Redirects with ModifyVariable where possible

--- common/src/main/java/dev/latvian/mods/kubejs/mixin/common/inject_resources/WorldStemMixin.java
+++ common/src/main/java/dev/latvian/mods/kubejs/mixin/common/inject_resources/WorldStemMixin.java
@@ -2,24 +2,17 @@ package dev.latvian.mods.kubejs.mixin.common.inject_resources;
 
 import dev.latvian.mods.kubejs.server.ServerScriptManager;
 import net.minecraft.server.WorldStem;
-import net.minecraft.server.packs.PackResources;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.resources.MultiPackResourceManager;
+import net.minecraft.server.packs.resources.CloseableResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.List;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(WorldStem.class)
 public abstract class WorldStemMixin {
 
-	@Redirect(method = "load", at = @At(
-			value = "NEW",
-			target = "net/minecraft/server/packs/resources/MultiPackResourceManager"
-	))
-	private static MultiPackResourceManager injectKubeJSPacks(PackType packType, List<PackResources> list) {
+	@ModifyVariable(method = "load", at = @At("STORE"))
+	private static CloseableResourceManager injectKubeJSPacks(CloseableResourceManager original) {
 		ServerScriptManager.instance = new ServerScriptManager();
-		return ServerScriptManager.instance.wrapResourceManager(packType, list);
+		return ServerScriptManager.instance.wrapResourceManager(original);
 	}
 }
