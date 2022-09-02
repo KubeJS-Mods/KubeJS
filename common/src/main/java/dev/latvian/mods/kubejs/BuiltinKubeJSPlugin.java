54ab2576392de31d0c44ad7d23f531f2904298b7
Max <maxh2709@gmail.com>
2022-09-02 18:57:42 +0200

    Update to Rhino 1802.2.0, bump version (this release breaks ProbeJS!)

--- common/src/main/java/dev/latvian/mods/kubejs/BuiltinKubeJSPlugin.java
+++ common/src/main/java/dev/latvian/mods/kubejs/BuiltinKubeJSPlugin.java
@@ -293,7 +293,7 @@ public class BuiltinKubeJSPlugin extends KubeJSPlugin {
 
 		event.addFunction("onEvent", args -> onEvent(event, args), null, IEventHandler.class);
 		event.addFunction("postEvent", args -> postEvent(event, args), String.class, Object.class, Boolean.class);
-		event.addFunction("java", args -> event.manager.loadJavaClass(event.scope, args), new Class[]{null});
+		event.addFunction("java", args -> event.manager.loadJavaClass(event, args), new Class[]{null});
 
 		event.add("JavaMath", Math.class);
 		event.add("ResourceLocation", ResourceLocation.class);
--- common/src/main/java/dev/latvian/mods/kubejs/util/ClassWrapper.java
+++ common/src/main/java/dev/latvian/mods/kubejs/util/ClassWrapper.java
@@ -1,13 +1,13 @@
 package dev.latvian.mods.kubejs.util;
 
-import dev.latvian.mods.rhino.Context;
 import dev.latvian.mods.rhino.NativeJavaClass;
 import dev.latvian.mods.rhino.Scriptable;
+import dev.latvian.mods.rhino.SharedContextData;
 import dev.latvian.mods.rhino.util.CustomJavaToJsWrapper;
 
 public record ClassWrapper<T>(Class<T> wrappedClass) implements CustomJavaToJsWrapper {
 	@Override
-	public Scriptable convertJavaToJs(Context cx, Scriptable scope, Class<?> staticType) {
+	public Scriptable convertJavaToJs(SharedContextData data, Scriptable scope, Class<?> staticType) {
 		return new NativeJavaClass(scope, wrappedClass);
 	}
 
