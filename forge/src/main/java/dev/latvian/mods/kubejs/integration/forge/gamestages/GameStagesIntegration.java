f44fe8ab5de4e72ac02876a732b9eb47ce1db0ee
LatvianModder <latvianmodder@gmail.com>
2022-09-05 15:02:19 +0300

    Made GameStagesIntegration.GROUP and events static

--- forge/src/main/java/dev/latvian/mods/kubejs/integration/forge/gamestages/GameStagesIntegration.java
+++ forge/src/main/java/dev/latvian/mods/kubejs/integration/forge/gamestages/GameStagesIntegration.java
@@ -3,7 +3,6 @@ package dev.latvian.mods.kubejs.integration.forge.gamestages;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.event.EventGroup;
 import dev.latvian.mods.kubejs.event.EventHandler;
-import dev.latvian.mods.kubejs.stages.NoStages;
 import dev.latvian.mods.kubejs.stages.StageCreationEvent;
 import dev.latvian.mods.kubejs.stages.Stages;
 import net.darkhax.gamestages.event.GameStageEvent;
@@ -14,25 +13,29 @@ import net.minecraftforge.common.MinecraftForge;
  */
 public class GameStagesIntegration extends KubeJSPlugin {
 
-	EventGroup group = EventGroup.of("GameStageEvents");
+	public static final EventGroup GROUP = EventGroup.of("GameStageEvents");
 
-	EventHandler STAGE_ADDED = group.server("stageAdded", () -> GameStageEventJS.class);
-	EventHandler STAGE_REMOVED = group.server("stageRemoved", () -> GameStageEventJS.class);
+	public static final EventHandler STAGE_ADDED = GROUP.server("stageAdded", () -> GameStageEventJS.class);
+	public static final EventHandler STAGE_REMOVED = GROUP.server("stageRemoved", () -> GameStageEventJS.class);
 
 	@Override
 	public void init() {
-		MinecraftForge.EVENT_BUS.<GameStageEvent.Added>addListener(e -> {
-			STAGE_ADDED.post(e.getStageName(), new GameStageEventJS(e));
-			Stages.invokeAdded(Stages.get(e.getEntity()), e.getStageName());
-		});
-		MinecraftForge.EVENT_BUS.<GameStageEvent.Removed>addListener(e -> {
-			STAGE_REMOVED.post(e.getStageName(), new GameStageEventJS(e));
-			Stages.invokeRemoved(Stages.get(e.getEntity()), e.getStageName());
-		});
-		Stages.overrideCreation(event -> event.setPlayerStages(new GameStagesWrapper(event.getPlayer())));
+		Stages.overrideCreation(GameStagesIntegration::override);
+		MinecraftForge.EVENT_BUS.addListener(GameStagesIntegration::stageAdded);
+		MinecraftForge.EVENT_BUS.addListener(GameStagesIntegration::stageRemoved);
 	}
 
-	private void override(StageCreationEvent event) {
-		event.setPlayerStages(NoStages.NULL_INSTANCE);
+	private static void override(StageCreationEvent event) {
+		event.setPlayerStages(new GameStagesWrapper(event.getPlayer()));
+	}
+
+	private static void stageAdded(GameStageEvent.Added event) {
+		STAGE_ADDED.post(event.getStageName(), new GameStageEventJS(event));
+		Stages.invokeAdded(Stages.get(event.getEntity()), event.getStageName());
+	}
+
+	private static void stageRemoved(GameStageEvent.Removed event) {
+		STAGE_REMOVED.post(event.getStageName(), new GameStageEventJS(event));
+		Stages.invokeRemoved(Stages.get(event.getEntity()), event.getStageName());
 	}
 }
