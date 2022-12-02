937236dae2b50a556c86e3a22b799faf7ebadd47
Christopher Roussel <christopher@impleri.net>
2022-12-01 21:03:52 -0600

    refactor: Remove unused import

--- common/src/main/java/dev/latvian/mods/kubejs/KubeJS.java
+++ common/src/main/java/dev/latvian/mods/kubejs/KubeJS.java
@@ -33,7 +33,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystemLoopException;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
