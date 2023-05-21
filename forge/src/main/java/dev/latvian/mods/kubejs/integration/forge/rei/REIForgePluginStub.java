0e0774ea88a6c866657450d8b06ab338340add2a
Max <maxh2709@gmail.com>
2023-05-21 15:57:43 +0200

    Update dependencies to fix build

--- forge/src/main/java/dev/latvian/mods/kubejs/integration/forge/rei/REIForgePluginStub.java
+++ forge/src/main/java/dev/latvian/mods/kubejs/integration/forge/rei/REIForgePluginStub.java
@@ -1,9 +1,8 @@
 package dev.latvian.mods.kubejs.integration.forge.rei;
 
 import dev.latvian.mods.kubejs.integration.rei.KubeJSREIPlugin;
-import me.shedaniel.rei.forge.REIPlugin;
-import net.minecraftforge.api.distmarker.Dist;
+import me.shedaniel.rei.forge.REIPluginClient;
 
-@REIPlugin(Dist.CLIENT)
+@REIPluginClient
 public class REIForgePluginStub extends KubeJSREIPlugin {
 }
