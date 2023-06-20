e92ec92961ffcd9ce07535f6b9b5553b745ba032
AnAwesomGuy <evanyweng@gmail.com>
2023-06-20 20:36:55 +0800

    made it into only one mixin file

--- common/src/main/java/dev/latvian/mods/kubejs/mixin/common/tools/shears/InteractShearsMixin.java
+++ common/src/main/java/dev/latvian/mods/kubejs/mixin/common/tools/shears/InteractShearsMixin.java
@@ -2,6 +2,8 @@ package dev.latvian.mods.kubejs.mixin.common.tools.shears;
 
 import dev.latvian.mods.kubejs.item.custom.ShearsItemBuilder;
 import net.minecraft.core.Registry;
+import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.animal.SnowGolem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.BeehiveBlock;
@@ -10,9 +12,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin({PumpkinBlock.class, BeehiveBlock.class})
-public abstract class BlockInteractShearsMixin {
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"), method = "use")
+@Mixin({BeehiveBlock.class, PumpkinBlock.class, Sheep.class, SnowGolem.class})
+public abstract class InteractShearsMixin {
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"), method = {"mobInteract", "use"})
     private boolean isShears(ItemStack stack, Item item) {
         return ShearsItemBuilder.SHEARS_ID_LIST.contains(Registry.ITEM.getKey(stack.getItem())) || stack.is(item);
     }
