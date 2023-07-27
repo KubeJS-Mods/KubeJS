6b64632382d9f0a49b97a7c9dca6939fa7a37c7e
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2023-07-27 21:04:30 +1200

    Document KubeJSRegistries (Utils.registries in JS)

--- common/src/main/java/dev/latvian/mods/kubejs/registry/KubeJSRegistries.java
+++ common/src/main/java/dev/latvian/mods/kubejs/registry/KubeJSRegistries.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import dev.architectury.registry.registries.Registrar;
 import dev.architectury.registry.registries.Registries;
 import dev.latvian.mods.kubejs.KubeJS;
+import dev.latvian.mods.kubejs.typings.Info;
 import dev.latvian.mods.rhino.util.HideFromJS;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
@@ -26,8 +27,10 @@ import net.minecraft.world.level.material.Fluid;
 import static net.minecraft.core.Registry.*;
 
 public class KubeJSRegistries {
+	@Info("Platform-agnostic wrapper of minecraft registries, can be used to register content or get objects from the registry")
 	public static final Registries REGISTRIES = Registries.get(KubeJS.MOD_ID);
 
+	@Info("Gets the registry associated with the id")
 	public static <T> Registrar<T> byId(ResourceLocation id) {
 		return genericRegistry(ResourceKey.createRegistryKey(id));
 	}
@@ -37,62 +40,77 @@ public class KubeJSRegistries {
 		return REGISTRIES.get(key);
 	}
 
+	@Info("Gets the block registrar")
 	public static Registrar<Block> blocks() {
 		return genericRegistry(BLOCK_REGISTRY);
 	}
 
+	@Info("Gets the attribute registrar")
 	public static Registrar<Attribute> attributes() {
 		return genericRegistry(ATTRIBUTE_REGISTRY);
 	}
 
+	@Info("Gets the block entity registrar")
 	public static Registrar<BlockEntityType<?>> blockEntities() {
 		return genericRegistry(BLOCK_ENTITY_TYPE_REGISTRY);
 	}
 
+	@Info("Gets the item registrar")
 	public static Registrar<Item> items() {
 		return genericRegistry(ITEM_REGISTRY);
 	}
 
+	@Info("Gets the fluid registrar")
 	public static Registrar<Fluid> fluids() {
 		return genericRegistry(FLUID_REGISTRY);
 	}
 
+	@Info("Gets the entity type registrar")
 	public static Registrar<EntityType<?>> entityTypes() {
 		return genericRegistry(ENTITY_TYPE_REGISTRY);
 	}
 
+	@Info("Gets the menu type registrar")
 	public static Registrar<MenuType<?>> menuTypes() {
 		return genericRegistry(MENU_REGISTRY);
 	}
 
+	@Info("Gets the sound event registrar")
 	public static Registrar<SoundEvent> soundEvents() {
 		return genericRegistry(SOUND_EVENT_REGISTRY);
 	}
 
+	@Info("Gets the recipe serializer registrar")
 	public static Registrar<RecipeSerializer<?>> recipeSerializers() {
 		return genericRegistry(RECIPE_SERIALIZER_REGISTRY);
 	}
 
+	@Info("Gets the biome registrar")
 	public static Registrar<Biome> biomes() {
 		return genericRegistry(BIOME_REGISTRY);
 	}
 
+	@Info("Gets the chunk generator registrar")
 	public static Registrar<Codec<? extends ChunkGenerator>> chunkGenerators() {
 		return genericRegistry(CHUNK_GENERATOR_REGISTRY);
 	}
 
+	@Info("Gets the block entity registrar")
 	public static Registrar<BlockEntityType<?>> blockEntityTypes() {
 		return genericRegistry(BLOCK_ENTITY_TYPE_REGISTRY);
 	}
 
+	@Info("Gets the potion registrar")
 	public static Registrar<Potion> potions() {
 		return genericRegistry(POTION_REGISTRY);
 	}
 
+	@Info("Gets the enchantment registrar")
 	public static Registrar<Enchantment> enchantments() {
 		return genericRegistry(ENCHANTMENT_REGISTRY);
 	}
 
+	@Info("Gets the mob effect registrar")
 	public static Registrar<MobEffect> mobEffects() {
 		return genericRegistry(MOB_EFFECT_REGISTRY);
 	}
