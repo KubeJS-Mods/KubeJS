ec277f09e580da0e10c8527ad165ee13396639fc
Max <maxh2709@gmail.com>
2023-08-02 18:15:14 +0200

    Use MixinExtras to make shears mixins less destructive

--- fabric/src/main/java/dev/latvian/mods/kubejs/core/mixin/fabric/tools/shears/TripWireBlockMixin.java
+++ fabric/src/main/java/dev/latvian/mods/kubejs/core/mixin/fabric/tools/shears/TripWireBlockMixin.java
@@ -1,18 +1,17 @@
 package dev.latvian.mods.kubejs.core.mixin.fabric.tools.shears;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.latvian.mods.kubejs.item.custom.ShearsItemBuilder;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.TripWireBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(TripWireBlock.class)
 public abstract class TripWireBlockMixin {
-	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"), method = "playerWillDestroy")
-	private boolean isShears(ItemStack stack, Item item) {
-		return stack.is(item) || (item == Items.SHEARS && ShearsItemBuilder.isCustomShears(stack));
+	@ModifyExpressionValue(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"), method = "playerWillDestroy")
+	private boolean kjs$isShears(boolean original, @Local Player player) {
+		return original || ShearsItemBuilder.isCustomShears(player.getMainHandItem());
 	}
 }
