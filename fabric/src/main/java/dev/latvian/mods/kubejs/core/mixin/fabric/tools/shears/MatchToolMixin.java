df8058da0b5bf9cfe4f548f19470c61fec5064be
Max <maxh2709@gmail.com>
2023-08-04 13:04:50 +0200

    Properly inject custom shears in MatchTool

--- fabric/src/main/java/dev/latvian/mods/kubejs/core/mixin/fabric/tools/shears/MatchToolMixin.java
+++ fabric/src/main/java/dev/latvian/mods/kubejs/core/mixin/fabric/tools/shears/MatchToolMixin.java
@@ -3,37 +3,35 @@ package dev.latvian.mods.kubejs.core.mixin.fabric.tools.shears;
 import dev.latvian.mods.kubejs.item.custom.ShearsItemBuilder;
 import dev.latvian.mods.kubejs.registry.RegistryInfo;
 import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.storage.loot.predicates.MatchTool;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Mixin(MatchTool.class)
 public abstract class MatchToolMixin {
 
-	// TODO (high): Instead of serialising and later deserialising the predicate,
-	//  can't we just... add all the custom shears to the predicate itself?
-	@ModifyVariable(at = @At("HEAD"), method = "<init>", argsOnly = true)
-	private static ItemPredicate shearsLoot(ItemPredicate predicate) {
-		var element = predicate.serializeToJson().deepCopy();
-		var object = element.isJsonObject() ? element.getAsJsonObject() : null;
+	@Shadow
+	@Final
+	ItemPredicate predicate;
 
-		if (object != null) {
-			var items = object.getAsJsonArray("items");
-
-			if (items != null) {
-				for (var jsonElement : items.deepCopy()) {
-					if (jsonElement.getAsString().equals("minecraft:shears")) {
-						for (var e : RegistryInfo.ITEM.objects.entrySet()) {
-							if (e.getValue() instanceof ShearsItemBuilder) {
-								items.add(e.getKey().toString());
-							}
-						}
-					}
+	@Inject(at = @At("RETURN"), method = "<init>")
+	private void kjs$injectCustomShears(CallbackInfo ci) {
+		if (predicate.items != null && predicate.items.contains(Items.SHEARS)) {
+			var set = new HashSet<>(predicate.items);
+			for (var e : RegistryInfo.ITEM.objects.entrySet()) {
+				if (e.getValue() instanceof ShearsItemBuilder builder) {
+					set.add(builder.get());
 				}
 			}
+			predicate.items = Set.copyOf(set);
 		}
-
-		return ItemPredicate.fromJson(element);
 	}
 }
