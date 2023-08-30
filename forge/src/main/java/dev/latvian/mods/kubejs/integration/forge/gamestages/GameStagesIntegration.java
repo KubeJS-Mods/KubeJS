742f5b6a6b3d146dfab46d23a45b6bfdc433006b
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2023-08-30 18:53:36 +1200

    Fix `Event handlers 'GameStageEvents.stageAdded' doesn't support extra id create!` caused when firing the event. Someone forgot to test!

--- forge/src/main/java/dev/latvian/mods/kubejs/integration/forge/gamestages/GameStagesIntegration.java
+++ forge/src/main/java/dev/latvian/mods/kubejs/integration/forge/gamestages/GameStagesIntegration.java
@@ -3,6 +3,7 @@ package dev.latvian.mods.kubejs.integration.forge.gamestages;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.event.EventGroup;
 import dev.latvian.mods.kubejs.event.EventHandler;
+import dev.latvian.mods.kubejs.event.Extra;
 import dev.latvian.mods.kubejs.stages.StageCreationEvent;
 import dev.latvian.mods.kubejs.stages.Stages;
 import net.darkhax.gamestages.event.GameStageEvent;
@@ -12,8 +13,8 @@ public class GameStagesIntegration extends KubeJSPlugin {
 
 	public static final EventGroup GROUP = EventGroup.of("GameStageEvents");
 
-	public static final EventHandler STAGE_ADDED = GROUP.common("stageAdded", () -> GameStageEventJS.class);
-	public static final EventHandler STAGE_REMOVED = GROUP.common("stageRemoved", () -> GameStageEventJS.class);
+	public static final EventHandler STAGE_ADDED = GROUP.common("stageAdded", () -> GameStageEventJS.class).extra(Extra.STRING);
+	public static final EventHandler STAGE_REMOVED = GROUP.common("stageRemoved", () -> GameStageEventJS.class).extra(Extra.STRING);
 
 	@Override
 	public void init() {
