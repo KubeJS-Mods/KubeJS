78d0d65b31fd463670e2dff261abd801151cb51b
LatvianModder <latvianmodder@gmail.com>
2023-10-19 17:30:19 +0300

    Fixed packmode bug causing scripts not to load at all in newly created directories

--- common/src/main/java/dev/latvian/mods/kubejs/script/ScriptFileInfo.java
+++ common/src/main/java/dev/latvian/mods/kubejs/script/ScriptFileInfo.java
@@ -65,7 +65,7 @@ public class ScriptFileInfo {
 
 		priority = Integer.parseInt(getProperty("priority", "0"));
 		ignored = getProperty("ignored", "false").equals("true") || getProperty("ignore", "false").equals("true");
-		packMode = getProperty("packmode", "default");
+		packMode = getProperty("packmode", "");
 		requiredMods.addAll(getProperties("requires"));
 	}
 
