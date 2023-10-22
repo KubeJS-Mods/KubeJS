07907d1edbb8397984cd2d0f4aedca2bc116cc69
Dietrich Friday <friday.dietrich@gmail.com>
2023-10-22 23:15:00 +0900

    "Thumbs.db" is, I believe, always has a capital 'T'.

--- common/src/main/java/dev/latvian/mods/kubejs/script/data/GeneratedResourcePack.java
+++ common/src/main/java/dev/latvian/mods/kubejs/script/data/GeneratedResourcePack.java
@@ -39,7 +39,7 @@ public abstract class GeneratedResourcePack implements ExportablePackResources {
 			try {
 				var fileName = p.getFileName().toString();
 
-				if (fileName.endsWith(".zip") || fileName.equals(".DS_Store") || fileName.equals("thumbs.db") || fileName.equals("desktop.ini")) {
+				if (fileName.endsWith(".zip") || fileName.equals(".DS_Store") || fileName.equals("Thumbs.db") || fileName.equals("desktop.ini")) {
 					return;
 				} else if (Files.isHidden(path)) {
 					ConsoleJS.STARTUP.error("Invisible file found: " + pathName + path.relativize(p).toString().replace('\\', '/'));
@@ -103,7 +103,7 @@ public abstract class GeneratedResourcePack implements ExportablePackResources {
 						int sindex = pathStr.lastIndexOf('/');
 						var fileName = sindex == -1 ? pathStr : pathStr.substring(sindex + 1);
 
-						if (fileName.endsWith(".zip") || fileName.equals(".DS_Store") || fileName.equals("thumbs.db") || fileName.equals("desktop.ini") || Files.isHidden(path)) {
+						if (fileName.endsWith(".zip") || fileName.equals(".DS_Store") || fileName.equals("Thumbs.db") || fileName.equals("desktop.ini") || Files.isHidden(path)) {
 							continue;
 						}
 
