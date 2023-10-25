63c862b304180a4d3a47c79e3dafabe357cda768
LatvianModder <latvianmodder@gmail.com>
2023-10-25 22:12:31 +0300

    Added renameItem, renameBlock, renameEntity methods in language event, error for invalid keys

--- common/src/main/java/dev/latvian/mods/kubejs/client/LangEventJS.java
+++ common/src/main/java/dev/latvian/mods/kubejs/client/LangEventJS.java
@@ -1,6 +1,11 @@
 package dev.latvian.mods.kubejs.client;
 
 import dev.latvian.mods.kubejs.event.EventJS;
+import dev.latvian.mods.kubejs.registry.RegistryInfo;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -20,6 +25,10 @@ public class LangEventJS extends EventJS {
 	}
 
 	public void add(String namespace, String key, String value) {
+		if (namespace == null || key == null || value == null || namespace.isEmpty() || key.isEmpty() || value.isEmpty()) {
+			throw new IllegalArgumentException("Invalid namespace, key or value: [" + namespace + ", " + key + ", " + value + "]");
+		}
+
 		map.put(new Key(namespace, lang, key), value);
 	}
 
@@ -35,5 +44,39 @@ public class LangEventJS extends EventJS {
 
 	public void addAll(Map<String, String> map) {
 		addAll("minecraft", map);
+	}
+
+	public void renameItem(ItemStack item, String name) {
+		if (item != null && !item.isEmpty()) {
+			var d = item.getDescriptionId();
+
+			if (d != null && !d.isEmpty()) {
+				add(item.kjs$getMod(), d, name);
+			}
+		}
+	}
+
+	public void renameBlock(Block block, String name) {
+		if (block != null && block != Blocks.AIR) {
+			var d = block.getDescriptionId();
+
+			if (d != null && !d.isEmpty()) {
+				add(block.kjs$getMod(), d, name);
+			}
+		}
+	}
+
+	public void renameEntity(EntityType<?> entityType, String name) {
+		if (entityType != null) {
+			var d = entityType.getDescriptionId();
+
+			if (d != null && !d.isEmpty()) {
+				var id = RegistryInfo.ENTITY_TYPE.getId(entityType);
+
+				if (id != null) {
+					add(id.getNamespace(), d, name);
+				}
+			}
+		}
 	}
 }
