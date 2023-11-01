f16ddab6ae70cb93193e46ff4e8838d02c6196e1
LatvianModder <latvianmodder@gmail.com>
2023-11-01 08:58:46 +0200

    Added better error handling for block entities

--- common/src/main/java/dev/latvian/mods/kubejs/block/entity/BlockEntityJSTicker.java
+++ common/src/main/java/dev/latvian/mods/kubejs/block/entity/BlockEntityJSTicker.java
@@ -1,15 +1,25 @@
 package dev.latvian.mods.kubejs.block.entity;
 
+import dev.latvian.mods.kubejs.util.ConsoleJS;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.state.BlockState;
 
-public record BlockEntityJSTicker(int frequency, int offset, BlockEntityCallback callback) implements BlockEntityTicker<BlockEntityJS> {
+public record BlockEntityJSTicker(BlockEntityInfo info, int frequency, int offset, BlockEntityCallback callback, boolean server) implements BlockEntityTicker<BlockEntityJS> {
 	@Override
 	public void tick(Level level, BlockPos blockPos, BlockState blockState, BlockEntityJS e) {
 		if (frequency <= 1 || e.tick % frequency == offset) {
-			callback.accept(e);
+			try {
+				callback.accept(e);
+			} catch (Exception ex) {
+				if (server) {
+					ConsoleJS.SERVER.error("Error while ticking KubeJS block entity '" + info.blockBuilder.id + "'", ex, null);
+				} else {
+					ConsoleJS.CLIENT.error("Error while ticking KubeJS block entity '" + info.blockBuilder.id + "'", ex, null);
+				}
+			}
+
 			e.postTick(true);
 		} else {
 			e.postTick(false);
