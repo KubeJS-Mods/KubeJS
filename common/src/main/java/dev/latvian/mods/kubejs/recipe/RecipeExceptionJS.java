df234faa00d4b8ef2e95b8be205083b6c0353138
Max <maxh2709@gmail.com>
2024-01-25 17:25:13 +0100

    Fix RecipeExceptionJS swallowing underlying errors

--- common/src/main/java/dev/latvian/mods/kubejs/recipe/RecipeExceptionJS.java
+++ common/src/main/java/dev/latvian/mods/kubejs/recipe/RecipeExceptionJS.java
@@ -15,7 +15,19 @@ public class RecipeExceptionJS extends IllegalArgumentException {
 
 	@Override
 	public String toString() {
-		return getLocalizedMessage();
+		var sb = new StringBuilder();
+		sb.append(getMessage());
+		if (error) {
+			sb.append(" [error]");
+		}
+
+		// append cause as well since RecipeExceptions can swallow underlying problems
+		if (getCause() != null) {
+			sb.append("\ncause: ");
+			sb.append(getCause());
+		}
+
+		return sb.toString();
 	}
 
 	public RecipeExceptionJS error() {
