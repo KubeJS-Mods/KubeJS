7855843c34c4655589ae301b3c29a894a013fc77
Hunter19823 <hspragg@asu.edu>
2024-03-04 20:27:46 -0700

    Created a CarpetBlockBuilder

---
+++ common/src/main/java/dev/latvian/mods/kubejs/block/custom/CarpetBlockBuilder.java
@@ -0,0 +1,40 @@
+package dev.latvian.mods.kubejs.block.custom;
+
+import dev.latvian.mods.kubejs.client.VariantBlockStateGenerator;
+import dev.latvian.mods.kubejs.generator.AssetJsonGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CarpetBlock;
+
+public class CarpetBlockBuilder extends ShapedBlockBuilder {
+	public CarpetBlockBuilder(ResourceLocation i) {
+		super(i, "_carpet");
+		tagBoth(BlockTags.WOOL_CARPETS.location());
+	}
+
+	@Override
+	public Block createObject() {
+		return new CarpetBlock(createProperties());
+	}
+
+	@Override
+	protected void generateBlockStateJson(VariantBlockStateGenerator bs) {
+		var mod = newID("block/", "").toString();
+		bs.variant("", (v) -> v.model(mod));
+	}
+
+	@Override
+	protected void generateBlockModelJsons(AssetJsonGenerator generator) {
+		var texture = textures.get("texture").getAsString();
+
+		generator.blockModel(id, m -> {
+			m.parent("minecraft:block/carpet");
+			m.texture("wool", texture);
+		});
+	}
+
+	public CarpetBlockBuilder texture(String texture) {
+		return (CarpetBlockBuilder) textureAll(texture);
+	}
+}
