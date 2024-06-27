de940e3352029d140543379a5f4f9e41af6baa42
LatvianModder <latvianmodder@gmail.com>
2024-06-27 20:12:04 +0300

    Updated neo

--- src/main/java/dev/latvian/mods/kubejs/ingredient/KubeJSIngredients.java
+++ src/main/java/dev/latvian/mods/kubejs/ingredient/KubeJSIngredients.java
@@ -1,7 +1,6 @@
 package dev.latvian.mods.kubejs.ingredient;
 
 import dev.latvian.mods.kubejs.KubeJS;
-import net.minecraft.network.codec.StreamCodec;
 import net.neoforged.neoforge.common.crafting.IngredientType;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
@@ -11,10 +10,9 @@ import java.util.function.Supplier;
 public interface KubeJSIngredients {
 	DeferredRegister<IngredientType<?>> REGISTRY = DeferredRegister.create(NeoForgeRegistries.Keys.INGREDIENT_TYPES, KubeJS.MOD_ID);
 
-	// FIXME: Neo bug
-	Supplier<IngredientType<WildcardIngredient>> WILDCARD = REGISTRY.register("wildcard", () -> new IngredientType<>(WildcardIngredient.CODEC, (StreamCodec) WildcardIngredient.STREAM_CODEC));
-	Supplier<IngredientType<TagIngredient>> TAG = REGISTRY.register("tag", () -> new IngredientType<>(TagIngredient.CODEC, (StreamCodec) TagIngredient.STREAM_CODEC));
-	Supplier<IngredientType<NamespaceIngredient>> NAMESPACE = REGISTRY.register("namespace", () -> new IngredientType<>(NamespaceIngredient.CODEC, (StreamCodec) NamespaceIngredient.STREAM_CODEC));
-	Supplier<IngredientType<RegExIngredient>> REGEX = REGISTRY.register("regex", () -> new IngredientType<>(RegExIngredient.CODEC, (StreamCodec) RegExIngredient.STREAM_CODEC));
+	Supplier<IngredientType<WildcardIngredient>> WILDCARD = REGISTRY.register("wildcard", () -> new IngredientType<>(WildcardIngredient.CODEC, WildcardIngredient.STREAM_CODEC));
+	Supplier<IngredientType<TagIngredient>> TAG = REGISTRY.register("tag", () -> new IngredientType<>(TagIngredient.CODEC, TagIngredient.STREAM_CODEC));
+	Supplier<IngredientType<NamespaceIngredient>> NAMESPACE = REGISTRY.register("namespace", () -> new IngredientType<>(NamespaceIngredient.CODEC, NamespaceIngredient.STREAM_CODEC));
+	Supplier<IngredientType<RegExIngredient>> REGEX = REGISTRY.register("regex", () -> new IngredientType<>(RegExIngredient.CODEC, RegExIngredient.STREAM_CODEC));
 	Supplier<IngredientType<CreativeTabIngredient>> CREATIVE_TAB = REGISTRY.register("creative_tab", () -> new IngredientType<>(CreativeTabIngredient.CODEC, CreativeTabIngredient.STREAM_CODEC));
 }
