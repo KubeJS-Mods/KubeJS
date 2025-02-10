eab9c5a59b58fc496481728a0c6c42d502f0d110
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-02-10 08:19:01 -0500

    Create MobEffectUtil.java

---
+++ src/main/java/dev/latvian/mods/kubejs/util/MobEffectUtil.java
@@ -0,0 +1,34 @@
+package dev.latvian.mods.kubejs.util;
+
+import net.minecraft.core.Holder;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public class MobEffectUtil {
+    @Contract("_ -> new")
+    public static @NotNull MobEffectInstance of(Holder<MobEffect> effect) {
+        return new MobEffectInstance(effect);
+    }
+
+    @Contract("_, _ -> new")
+    public static @NotNull MobEffectInstance of(Holder<MobEffect> effect, int duration) {
+        return new MobEffectInstance(effect, duration);
+    }
+
+    @Contract("_, _, _ -> new")
+    public static @NotNull MobEffectInstance of(Holder<MobEffect> effect, int duration, int amplifier) {
+        return new MobEffectInstance(effect, duration, amplifier);
+    }
+
+    @Contract("_, _, _, _, _ -> new")
+    public static @NotNull MobEffectInstance of(Holder<MobEffect> effect, int duration, int amplifier, boolean ambient, boolean visible) {
+        return new MobEffectInstance(effect, duration, amplifier, ambient, visible);
+    }
+
+    @Contract("_, _, _, _, _, _ -> new")
+    public static @NotNull MobEffectInstance of(Holder<MobEffect> effect, int duration, int amplifier, boolean ambient, boolean visible, boolean showIcon) {
+        return new MobEffectInstance(effect, duration, amplifier, ambient, visible, showIcon);
+    }
+}
