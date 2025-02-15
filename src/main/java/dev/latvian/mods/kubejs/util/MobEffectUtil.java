af18f9029bae45affb89bb6a2d2652b9b67cb8cc
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-02-14 19:09:57 -0500

    Add @Info annotations

--- src/main/java/dev/latvian/mods/kubejs/util/MobEffectUtil.java
+++ src/main/java/dev/latvian/mods/kubejs/util/MobEffectUtil.java
@@ -1,5 +1,6 @@
 package dev.latvian.mods.kubejs.util;
 
+import dev.latvian.mods.kubejs.typings.Info;
 import net.minecraft.core.Holder;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -8,31 +9,37 @@ import org.jetbrains.annotations.NotNull;
 
 public class MobEffectUtil {
 	@Contract("_ -> new")
+	@Info("Creates a new instance from the old one")
 	public static @NotNull MobEffectInstance of(MobEffectInstance oldInstance) {
 		return new MobEffectInstance(oldInstance)
 	}
 
     @Contract("_ -> new")
+	@Info("Creates an instance for the given effect")
     public static @NotNull MobEffectInstance of(Holder<MobEffect> effect) {
         return new MobEffectInstance(effect);
     }
 
     @Contract("_, _ -> new")
+	@Info("Creates an instance for the given effect and duration")
     public static @NotNull MobEffectInstance of(Holder<MobEffect> effect, int duration) {
         return new MobEffectInstance(effect, duration);
     }
 
     @Contract("_, _, _ -> new")
+	@Info("Creates an instance for the given effect, duration and amplifier")
     public static @NotNull MobEffectInstance of(Holder<MobEffect> effect, int duration, int amplifier) {
         return new MobEffectInstance(effect, duration, amplifier);
     }
 
     @Contract("_, _, _, _, _ -> new")
+	@Info("Creates an instance for the given effect, duration, amplifier, ambient, and visible to the HUD")
     public static @NotNull MobEffectInstance of(Holder<MobEffect> effect, int duration, int amplifier, boolean ambient, boolean visible) {
         return new MobEffectInstance(effect, duration, amplifier, ambient, visible);
     }
 
     @Contract("_, _, _, _, _, _ -> new")
+	@Info("Creates an instance for the given effect, duration, amplifier, ambient, visible to the HUD, and to show the icon")
     public static @NotNull MobEffectInstance of(Holder<MobEffect> effect, int duration, int amplifier, boolean ambient, boolean visible, boolean showIcon) {
         return new MobEffectInstance(effect, duration, amplifier, ambient, visible, showIcon);
     }
