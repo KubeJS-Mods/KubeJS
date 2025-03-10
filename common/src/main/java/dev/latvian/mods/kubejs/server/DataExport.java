f84e1637f446616474127734d09678b78a85811a
max. <max@someone.ky>
2025-03-10 21:48:47 +0100

    backport 47386b68ecace68e394af3bbcfd20b2e53f00b4d to 1.20

--- common/src/main/java/dev/latvian/mods/kubejs/server/DataExport.java
+++ common/src/main/java/dev/latvian/mods/kubejs/server/DataExport.java
@@ -191,7 +191,7 @@ public class DataExport {
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
-			}, Util.ioPool());
+			}, Util.backgroundExecutor());
 		}
 
 		CompletableFuture.allOf(arr).join();
