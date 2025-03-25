0a6e14d6b992c3d349d981681a601d9b4677a278
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-25 08:06:02 -0400

    I forgot a ; in the binding

--- src/main/java/dev/latvian/mods/kubejs/plugin/builtin/BuiltinKubeJSPlugin.java
+++ src/main/java/dev/latvian/mods/kubejs/plugin/builtin/BuiltinKubeJSPlugin.java
@@ -472,7 +472,7 @@ public class BuiltinKubeJSPlugin implements KubeJSPlugin {
 
 		bindings.add("NativeEvents", NativeEventWrapper.class);
 
-		bindings.add("MobEffectInstance", MobEffectUtil.class)
+		bindings.add("MobEffectInstance", MobEffectUtil.class);
 	}
 
 	@Override
