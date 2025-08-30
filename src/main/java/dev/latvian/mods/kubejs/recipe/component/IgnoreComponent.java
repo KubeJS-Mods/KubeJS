911a2137e7e7eb05f66c565a8e8f9ab742892929
LatvianModder <latvianmodder@gmail.com>
2025-08-30 18:46:50 +0300

    Maybe fixed either component again

--- src/main/java/dev/latvian/mods/kubejs/recipe/component/IgnoreComponent.java
+++ src/main/java/dev/latvian/mods/kubejs/recipe/component/IgnoreComponent.java
@@ -1,18 +1,14 @@
 package dev.latvian.mods.kubejs.recipe.component;
 
-import com.google.gson.JsonObject;
 import com.mojang.serialization.Codec;
 import dev.latvian.mods.kubejs.KubeJS;
-import dev.latvian.mods.kubejs.recipe.KubeRecipe;
 import dev.latvian.mods.kubejs.recipe.RecipeScriptContext;
-import dev.latvian.mods.kubejs.util.OpsContainer;
 import dev.latvian.mods.rhino.type.TypeInfo;
 
 public enum IgnoreComponent implements RecipeComponent<Object> {
 	INSTANCE;
 
-	public static final Object OBJECT = new Object();
-	public static final Codec<Object> CODEC = Codec.unit(OBJECT);
+	public static final Codec<Object> CODEC = Codec.unit(INSTANCE);
 	public static final RecipeComponentType<Object> TYPE = RecipeComponentType.unit(KubeJS.id("ignore"), IgnoreComponent.INSTANCE);
 
 	@Override
@@ -22,7 +18,7 @@ public enum IgnoreComponent implements RecipeComponent<Object> {
 
 	@Override
 	public Codec<Object> codec() {
-		return CODEC;
+		throw new IllegalStateException();
 	}
 
 	@Override
@@ -32,15 +28,7 @@ public enum IgnoreComponent implements RecipeComponent<Object> {
 
 	@Override
 	public Object wrap(RecipeScriptContext cx, Object from) {
-		return OBJECT;
-	}
-
-	@Override
-	public void writeToJson(KubeRecipe recipe, RecipeComponentValue<Object> cv, JsonObject json) {
-	}
-
-	@Override
-	public void readFromJson(KubeRecipe recipe, RecipeComponentValue<Object> cv, JsonObject json) {
+		return from;
 	}
 
 	@Override
@@ -54,7 +42,7 @@ public enum IgnoreComponent implements RecipeComponent<Object> {
 
 	@Override
 	public boolean isEmpty(Object value) {
-		return true;
+		return value == null;
 	}
 
 	@Override
@@ -64,11 +52,6 @@ public enum IgnoreComponent implements RecipeComponent<Object> {
 
 	@Override
 	public void buildUniqueId(UniqueIdBuilder builder, Object value) {
-	}
-
-	@Override
-	public String toString(OpsContainer ops, Object value) {
-		return "ignore";
 	}
 
 	@Override
