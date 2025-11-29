1b5ff08bb1d93564a33f91bc72da4e4f2cbec132
Notenoughmail <78008321+Notenoughmail@users.noreply.github.com>
2025-11-29 16:42:29 -0700

    Respect `override_type` of Schemas (#1077)
    * ow
    
    * Cache that

--- src/main/java/dev/latvian/mods/kubejs/recipe/KubeRecipe.java
+++ src/main/java/dev/latvian/mods/kubejs/recipe/KubeRecipe.java
@@ -515,7 +515,7 @@ public class KubeRecipe implements RecipeLikeKJS, CustomJavaToJsWrapper {
 			}
 
 			if (newRecipe) {
-				json.addProperty("type", getSerializationTypeFunction().idString);
+				json.addProperty("type", getSerializationTypeFunction().schemaType.serializerType);
 			}
 
 			if (type.event.stageSerializer != null && json.has(KubeJSCraftingRecipe.STAGE_KEY) && !type.idString.equals("recipestages:stage")) {
