af5694228d7c556bb545ce09f73f30ef11e24922
max. <max@someone.ky>
2025-12-05 22:23:21 +0100

    disambiguate CompoundTag array puts

--- src/main/java/dev/latvian/mods/kubejs/core/mixin/CompoundTagMixin.java
+++ src/main/java/dev/latvian/mods/kubejs/core/mixin/CompoundTagMixin.java
@@ -5,6 +5,7 @@ import dev.latvian.mods.rhino.NativeJavaMap;
 import dev.latvian.mods.rhino.Scriptable;
 import dev.latvian.mods.rhino.type.TypeInfo;
 import dev.latvian.mods.rhino.util.CustomJavaToJsWrapper;
+import dev.latvian.mods.rhino.util.HideFromJS;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import org.spongepowered.asm.mixin.Final;
@@ -12,6 +13,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
+import java.util.List;
 import java.util.Map;
 
 @Mixin(CompoundTag.class)
@@ -27,4 +29,16 @@ public abstract class CompoundTagMixin implements CustomJavaToJsWrapper {
 	public Scriptable convertJavaToJs(Context cx, Scriptable scope, TypeInfo target) {
 		return new NativeJavaMap(cx, scope, this, tags, KJS$MAP_TYPE);
 	}
+
+	@Shadow
+	@HideFromJS
+	public abstract void putByteArray(String key, List<Byte> value);
+
+	@Shadow
+	@HideFromJS
+	public abstract void putIntArray(String key, List<Integer> value);
+
+	@Shadow
+	@HideFromJS
+	public abstract void putLongArray(String key, List<Long> value);
 }
