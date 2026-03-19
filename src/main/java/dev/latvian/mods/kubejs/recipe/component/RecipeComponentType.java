1720b1774d18f2637f92f9d0a486630d31e6286a
max. <max@someone.ky>
2026-03-19 01:13:50 +0100

    Remove unnecessary component type registry

--- src/main/java/dev/latvian/mods/kubejs/recipe/component/RecipeComponentType.java
+++ src/main/java/dev/latvian/mods/kubejs/recipe/component/RecipeComponentType.java
@@ -2,15 +2,17 @@ package dev.latvian.mods.kubejs.recipe.component;
 
 import com.mojang.serialization.MapCodec;
 import dev.latvian.mods.kubejs.KubeJS;
-import dev.latvian.mods.kubejs.registry.KubeJSRegistries;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import org.jetbrains.annotations.ApiStatus;
 
 @FunctionalInterface
 public interface RecipeComponentType<T> {
+	ResourceKey<Registry<RecipeComponentType<?>>> REGISTRY_KEY = ResourceKey.createRegistryKey(Identifier.fromNamespaceAndPath("kubejs", "recipe_component_type"));
+
 	static ResourceKey<RecipeComponentType<?>> key(Identifier id) {
-		return ResourceKey.create(KubeJSRegistries.RECIPE_COMPONENT_TYPE, id);
+		return ResourceKey.create(REGISTRY_KEY, id);
 	}
 
 	@ApiStatus.Internal
