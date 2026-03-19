dce69ccd391e12a98ae30f82e38464e8a71fe331
max. <max@someone.ky>
2026-03-19 01:16:18 +0100

    fix stack size mixin

--- src/main/java/dev/latvian/mods/kubejs/core/mixin/DataComponentsMixin.java
+++ src/main/java/dev/latvian/mods/kubejs/core/mixin/DataComponentsMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(DataComponents.class)
 public class DataComponentsMixin {
-	@ModifyConstant(method = "<clinit>", constant = @Constant(intValue = 64))
+	@ModifyConstant(method = "lambda$static$1", constant = @Constant(intValue = 99))
 	private static int kjs$maxStackSize(int original) {
 		return CommonProperties.get().getMaxStackSize(original);
 	}
